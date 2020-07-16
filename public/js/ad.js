092429937afcd35d64b55e8613abd0b6403b5386
unknown <j-p.koehl>
2020-07-16 15:08:44 +0200

    Gestion des annonces, formulaire de creation et edition

---
+++ public/js/ad.js
@@ -0,0 +1,31 @@
+$('#add-image').click(function() {
+    // Je récupère le numéro des futures champs que je vais créer
+    const index = +$('#widget-counter').val();
+
+    // Je récupère le prototype des entrées
+    const tmpl = $('#ad_images').data('prototype').replace(/__name__/g, index);
+
+    // J'injecte ce code au sein de la div
+    $('#ad_images').append(tmpl);
+
+    $('#widget-counter').val(index + 1);
+
+    // Je gère le button supprimer
+    handleDeleteButtons();
+});
+
+function handleDeleteButtons() {
+    $('button[data-action="delete"]').click(function() {
+        const target = this.dataset.target;
+        $(target).remove();
+    })
+}
+
+function updateCounter() {
+    const count = +$('#ad_images div.form-group').length;
+
+    $('#widgets-counter').val(count);
+}
+
+updateCounter();
+handleDeleteButtons();

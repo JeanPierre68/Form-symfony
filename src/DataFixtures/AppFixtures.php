7f39fa4beed7377b5deb2d50cfcf9ebd76e29423
unknown <j-p.koehl>
2020-07-20 00:00:40 +0200

    Ajout de la logique des roles et de la sécurité

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -21,6 +22,22 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Jean')
+                  ->setLastName('KOEHL')
+                  ->setEmail('j.koehl@orange.fr')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setPicture('https://randomuser.me/api/portraits/men/68.jpg')
+                  ->setIntroduction($faker->sentence())
+                  ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(5)) .'</p>')
+                  ->addUserRole($adminRole);
+
+        $manager->persist($adminUser);
 
         // Nous gérons les utilisateurs
         $users = [];
